1426a892853cee8b214d37991bdb9b1b8f695846
huangcn@123 <http://10.11.20.6>
2018-11-30 16:34:49 +0800

    增加WebSocket的兼容性 加入SpringSecurity

--- websocket/src/main/java/com/hcnay/websocket/config/WebSocketConfig.java
+++ websocket/src/main/java/com/hcnay/websocket/config/WebSocketConfig.java
@@ -2,13 +2,34 @@ package com.hcnay.websocket.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-
+@EnableWebSocketMessageBroker
 @Configuration
-public class WebSocketConfig {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     //创建服务器断点
     @Bean
     public ServerEndpointExporter serverEndpointExporter(){
         return new ServerEndpointExporter();
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //增加一个聊天服务端点
+        registry.addEndpoint("/sockte").withSockJS();
+        //增加一个用户服务端点
+        registry.addEndpoint("/wsuser").withSockJS();
+    }
+
+    //定义服务端点请求和订阅前缀
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //客户端订阅路径前缀
+        registry.enableSimpleBroker("/sub","/queue");
+        //服务端点请求前缀
+        registry.setApplicationDestinationPrefixes("/request");
     }
 }
